c8ded9eb267160d76a74f4cc2177d934eb7c9d2c
erictwalker18 <erictwalker18@gmail.com>
2017-02-18 10:46:33 -0600

    Added rest of stuff

--- nono-server.py
+++ nono-server.py
@@ -20,7 +20,7 @@ def get_index():
     if not solver_id or solver_id not in solvers_table:
         solver_id = create_new_solver_id(request)
         resp.set_cookie('solver_id', solver_id)
-        solvers_table[solver_[id] = Solver()
+        solvers_table[solver_id] = Solver()
     return resp
 
 # delete a solver's solve history
@@ -46,115 +46,129 @@ def get_puzzle_file():
         response = {'success': True, 'puzzle_id': puzzle_id, 'puzzle_file': puzzle_file, 'stats': {'puzzle_score': get_puzzle_score(puzzle_id)}}
     return json.dumps(response)
 
-
-
-# get ID of a puzzle with fewest or tied for fewest logs in DB
-def get_next_puzzle_id():
-    query = ('SELECT puzzle_id FROM nono_puzzles_by_id WHERE num_solves IN (SELECT min(num_solves) FROM nono_puzzles_by_id )', ())
-    rows = select_from_database(query)
-    puzzle_id = rows[0][0]
-    return puzzle_id
-
-# load puzzle file from db given its ID
-def get_puzzle_file_from_database(puzzle_id):
-    query = ('SELECT puzzle_file FROM nono_puzzles_by_id WHERE puzzle_id = %s', (puzzle_id,))
-    rows = select_from_database(query)
-    puzzle_file = rows[0][0]
-    return puzzle_file
-
-# get the puzzle_id associated with a given solve_id
-def get_solve_info(solve_id):
-    query = ('SELECT puzzle_id, mturk_token FROM nono_solve_info WHERE solve_id = %s', (solve_id,))
-    rows = select_from_database(query)
-    if len(rows) > 0:
-        return rows[0]
-    else:
-        return None
-
-# load a solve log file into the DB
-def init_new_solve_info(solve_id, puzzle_id):
-    # add an entry to solve_info
-    mturk_token = compute_mturk_token(solve_id)
-    #status is 0 - awaiting response
-    status = "0"
-    query = ("INSERT INTO nono_solve_info (solve_id, puzzle_id, mturk_token, status) VALUES (%s, %s, %s, %s)" , (solve_id, puzzle_id, mturk_token, status))
-    insert_into_database(query)
-
-# see if an mturk_token corresponds to a log file
-def verify_mturk_token(mturk_token):
-    query = ('SELECT COUNT (*) FROM nono_solve_logs WHERE nono_solve_logs.solve_id = nono_solve_info.solve_id AND nono_solve_info.mturk_token = %s', (mturk_token,))
-    rows = select_from_database(query)
-    return rows[0] > 0
-
-# load a solve log file into the DB
-def submit_log_file(solve_id, puzzle_id, log_file, status):
-    # add each move in the log to nono_solve_logs
-    log_file = log_file.strip()
-    moves = log_file.split('\n')
-    for move_num in range(len(moves)):
-        line = moves[move_num]
-        split = line.split(' ')
-        timestamp = split[0]
-        move = ' '.join(split[1:])
-        query = ('INSERT INTO nono_solve_logs VALUES(%s, %s, %s, %s)', (solve_id, move_num, timestamp, move))
-        insert_into_database(query)
-    # update the nono_solve_info table to record the type of response (completed or gave up)
-    query = ('UPDATE nono_solve_info SET status = %s WHERE solve_id = %s', (status, solve_id))
-    insert_into_database(query)
-    # if they solved it, increment num_solves for the puzzle_id
-    if status == 1:
-        query = ('UPDATE nono_puzzles_by_id SET num_solves = (num_solves + 1) WHERE puzzle_id IN (SELECT puzzle_id FROM nono_solve_info WHERE solve_id = %s)', (solve_id,))
-        insert_into_database(query)
-
-# serve puzzles to clients
-@app.route('/puzzle-file', methods=['GET'])
-def get_puzzle_file():
-    puzzle_id = get_next_puzzle_id()
-    puzzle_file = get_puzzle_file_from_database(puzzle_id)
-    solve_id = compute_solve_id(puzzle_id)
-    init_new_solve_info(solve_id, puzzle_id)
-    response = {'success': True, 'solve_id': solve_id, 'puzzle_file': puzzle_file}
-    return json.dumps(response)
-
 # receive new solve log file from client
-@app.route('/log-file', methods=['POST'])
+@app.route('/log', methods=['POST'])
 def post_log_file():
     try:
-        request = json.loads(flask.request.data.decode('utf-8'))
-        solve_id = request['solve_id']
-        status = request['status']
-        solve_info = get_solve_info(solve_id)
-        # see if they send a valid solve_id
-        if solve_info:
-            log_file = request['log_file']
-            puzzle_id, mturk_token = solve_info
-            # see if the log is valid in light of whether or not they purport to have solved it
-            puzzle_file = get_puzzle_file_from_database(puzzle_id)
-            if solve_log_is_valid(puzzle_file, log_file, status):
-                if status == 1:
-                    response = {'success': True, 'mturk_token': mturk_token}
-                else:
-                    response = {'success': True}
-                # put log file in database
-                submit_log_file(solve_id, puzzle_id, log_file, status)
+        request_json = json.loads(flask.request.data.decode('utf-8'))
+        status = request_json['status']
+        log_file = request_json['log_file'].strip()
+        puzzle_id = request_json['puzzle_id']
+        puzzle_file = get_puzzle_file_from_database(puzzle_id)
+        # see if the log is valid in light of whether or not they purport to have solved it
+        if solve_log_is_valid(puzzle_file, log_file, status):
+            solver_id = request.cookies.get('solver_id')
+            if not solver_id:
+                response = {'success': False, 'message': 'No solver_id set!'}
             else:
-                response = {'success': False, 'message': "Invalid solve log! What are you up to..."}
+                stats = update_solvers_table(solver_id, puzzle_id, log_file, status)
+                response = {'success': True, 'stats': {'log_score': get_log_score(log_file), 'solver_score': solvers_table[solver_id].get_solver_score()}}
         else:
-            response = {'success': False, 'message': "Invalid solve_id! You sly dog..."}
-    except json.decoder.JSONDecodeError:
+            response = {'success': False, 'message': "Invalid solve log! What are you up to..."}
+    except json.decoder.JSONDecodeError as e:
         response = {'success': False, 'message': "Invalid JSON! What are you up to..."}
     # send response
     return json.dumps(response)
 
-def select_from_database(query):
-    print(query)
-    cursor.execute(query[0], query[1])
-    return cursor.fetchall()
+### DATA ###
 
-def insert_into_database(query):
-    print(query)
-    cursor.execute(query[0], query[1])
-    connection.commit()
+# dictionary that stores info about each solver
+solvers_table = {}
+
+# ideal score
+ideal_score = 500
+max_score = 1000 # TODO: fine-tune this
+norm_spread = 5 # TODO: fine-tune this
+mle = MLE(max_score, norm_spread)
+
+# correlation coefficients
+wwf_coef = 6.51
+wwf2_coef = -0.01
+puzzle_score_offset = 221.89
+time_taken_coef = .5
+num_moves_coef = 6
+
+# object to store info about a solver
+class Solver:
+    def __init__(self):
+        self.true_skill = ideal_score
+        self.puzzle_scores = []
+        self.solve_scores = []
+        self.completed_puzzles = set()
+    def update(self, puzzle_id, puzzle_score, solve_score):
+        self.completed_puzzles.add(puzzle_id)
+        self.puzzle_scores.append(puzzle_score)
+        self.solve_scores.append(solve_score)
+        self.true_skill = mle.get_new_true_skill(self.true_skill, self.solve_scores, self.puzzle_scores)
+    def get_solver_score(self):
+        return self.true_skill
+
+### HELPER FUNCTIONS ###
+
+# create a new cookie value to remember the solver by
+def create_new_solver_id(request):
+    food = str(request.user_agent) + str(time.time())
+    m = hashlib.md5()
+    m.update(food.encode('UTF-8'))
+    return m.hexdigest()
+
+# update the score of a solver after they've solved a puzzle
+def update_solvers_table(solver_id, puzzle_id, log_file, status):
+    solver = solvers_table[solver_id]
+    puzzle_score = get_puzzle_score(puzzle_id)
+    log_score = get_log_score(log_file)
+    print("puzzle id: " + str(puzzle_id))
+    print("puzzle score: " + str(puzzle_score))
+    print("log score: " + str(log_score))
+    solver.update(puzzle_id, puzzle_score, log_score)
+
+# gets id of a good next puzzle for a solver based on their solver score
+def get_appropriate_puzzle_id(solver_id):
+    solver = solvers_table[solver_id]
+    target_puzzle_score = solver.get_solver_score()
+    query = ("SELECT puzzle_id FROM nono_puzzles ORDER BY ABS(((6.51*difficulty) - (0.01*(difficulty^2)) + 221.89) - %s) LIMIT 500;", (target_puzzle_score,))
+    rows = select_from_database(query)
+    # makes sure user doesn't receive already solved puzzle
+    i=0
+    while rows[i][0] in solver.completed_puzzles:
+        print("skipping...")
+        i = i+1
+    return rows[i][0]
+
+def get_puzzle_score(puzzle_id):
+    query = ("SELECT difficulty FROM puzzles WHERE puzzle_id = '%s';", (puzzle_id,))
+    rows = select_from_database(query)
+    difficulty = int(rows[0][0])
+    return (wwf_coef * difficulty) + (wwf2_coef * difficulty * difficulty) + puzzle_score_offset
+
+def get_log_score(log_file):
+    moves = log_file.split('\n')
+    num_moves = len(moves)
+    first_move = moves[0]
+    last_move = moves[-1]
+    time_taken = (int(last_move.split(' ')[0]) - int(first_move.split(' ')[0]))/1000
+    return (time_taken_coef * time_taken) + (num_moves_coef * num_moves)
+
+# load puzzle file from db given its ID
+def get_puzzle_file_from_database(puzzle_id):
+    query = ("SELECT puzzle_file FROM nono_puzzles WHERE puzzle_id = '%s';", (puzzle_id,))
+    rows = select_from_database(query)
+    puzzle_file = rows[0][0]
+    return puzzle_file
+
+def select_from_database(query):
+    try:
+        # print("QUERY: " + str(query))
+        connection = psycopg2.connect(user=config.username, password=config.password)
+        cursor = connection.cursor()
+        cursor.execute(query[0], query[1])
+        rows = cursor.fetchall()
+        # print("ROWS: " + str(rows))
+        connection.close()
+        return rows
+    except Exception as e:
+        print(e)
+        exit(1)
 
 if __name__ == '__main__':
     try:
